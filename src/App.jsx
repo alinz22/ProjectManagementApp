6d379eee80ababf6464c314c0473e44e44a5d442
Alan Mong <alanmongx@gmail.com>
2024-06-27 18:30:19 -0700

    implement screen when no project is selected and add creation project screen to both the side button and main button

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,36 @@
+import { useState } from "react";
+
 import ProjectsSidebar from "./components/ProjectsSideBar";
 import NewProject from "./components/NewProject";
+import NoProjectSelected from "./components/NoProjectSelected";
 
 function App() {
+  const [projectState, setProjectSate] = useState({
+    selectedProjectID: undefined,
+    projects: [],
+  });
+
+  function handleStartAddProject() {
+    setProjectSate((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectID: null,
+      };
+    });
+  }
+
+  let content;
+
+  if (projectState.selectedProjectID === null) {
+    content = <NewProject />;
+  } else if (projectState.selectedProjectID === undefined) {
+    content = <NoProjectSelected onStartAddProject={handleStartAddProject} />;
+  }
+
   return (
     <main className="h-screen my-8  flex gap-8">
-      <ProjectsSidebar />
-      <NewProject />
+      <ProjectsSidebar onStartAddProject={handleStartAddProject} />
+      {content}
     </main>
   );
 }
